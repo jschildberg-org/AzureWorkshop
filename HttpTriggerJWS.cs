4fc4be720e179fb173993f69af1b11347f91ee84
jschildberg <jeff.schildberg@pascuayaqui-nsn.gov>
2025-10-15 20:22:50 +0000

    New project

---
+++ HttpTriggerJWS.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace JWS.Function;
+
+public class HttpTriggerJWS
+{
+    private readonly ILogger<HttpTriggerJWS> _logger;
+
+    public HttpTriggerJWS(ILogger<HttpTriggerJWS> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("HttpTriggerJWS")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+}
